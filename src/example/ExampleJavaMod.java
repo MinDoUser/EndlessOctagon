9d43cb27e17ab1201ef92c0ff6a5f220e9c91404
MinDoUser <94752757+MinDoUser@users.noreply.github.com>
2022-07-04 14:00:52 +0200

    Update ExampleJavaMod.java

--- src/example/ExampleJavaMod.java
+++ src/example/ExampleJavaMod.java
@@ -8,6 +8,7 @@ import mindustry.game.EventType.*;
 import mindustry.gen.*;
 import mindustry.mod.*;
 import mindustry.ui.dialogs.*;
+import mindustry.world.meta.*;
 
 public class ExampleJavaMod extends Mod{
 
