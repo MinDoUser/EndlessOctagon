fc7dc00a45ee9776ca7f941faab803e36306f30f
MinDoUser <94752757+MinDoUser@users.noreply.github.com>
2022-07-04 14:08:53 +0200

    Create EndlessOctagonMod.java

---
+++ src/endlessOctagon/EndlessOctagonMod.java
@@ -0,0 +1,17 @@
+package endlessOctagon;
+
+import arc.util.*;
+
+import mindustry.mod.*;
+
+public class EndlessOctagonMod extends Mod{
+
+    public EndlessOctagonMod(){
+      Log.info("Mod constructor loaded...");
+    }
+  
+  @Override
+  public void loadContent(){
+    
+  }
+}
