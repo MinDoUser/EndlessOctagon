66c3f56dfe7627de30b9b38b01d126e6c886d7d5
MinDoUser <94752757+MinDoUser@users.noreply.github.com>
2022-07-04 18:13:50 +0200

    Create EOItems.java

---
+++ src/endlessOctagon/content/EOItems.java
@@ -0,0 +1,18 @@
+package endlessOctagon.content;
+
+import arc.graphics.*;
+import mindustry.ctype.*;
+import mindustry.type.*;
+
+public class EOItems implements ContentList{
+  public static final Color 
+    OXA_COLOR = Color.valueOf(eab678");
+    public static Item oxa;
+  public final void load(){
+    oxa = new Item("oxa", OXA_COLOR){{
+            hardness = 1;
+            cost = 0.75f;
+            alwaysUnlocked = false;
+        }};
+  }
+}
