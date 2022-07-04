b39ad5a69fdc3d11e6cf73d3ef5dec60c45a5ec4
MinDoUser <94752757+MinDoUser@users.noreply.github.com>
2022-07-04 17:11:04 +0200

    Create SolarCrafter.java

---
+++ src/endlessOctagon/world/blocks/crafter/SolarCrafter.java
@@ -0,0 +1,53 @@
+package endlessOctagon.world.blocks.carfter;
+
+import mindustry.world.blocks.production.*;
+
+import arc.*;
+import mindustry.graphics.*;
+import mindustry.ui.*;
+import mindustry.world.meta.*;
+
+import static mindustry.Vars.*;
+
+
+/** A Crafter that works with energy of the sun.
+* It won't work when the sun isn't shining.
+*/
+public class SolarCrafter extends GenericCrafter{
+  
+  public SolarCrafter(String name){
+    super(name);
+  }
+  
+      @Override
+    public void setBars(){
+        super.setBars();
+
+        bars.add("efficiency", (SolarCrafterBuild entity) ->
+            new Bar(() ->
+            Core.bundle.format("bar.efficiency", (int)(entity.efficiencyScale() * 100)),
+            () -> Pal.lightOrange,
+            entity::efficiencyScale));
+    }
+  
+    public class SolarCrafterBuild extends GenericCrafterBuild{
+      @Override
+        public float getProgressIncrease(float base){
+            return super.getProgressIncrease(base) * efficiencyScale();
+        }
+
+        public float efficiencyScale(){
+            return baseEfficiency + getEfficiency();
+        }
+      
+      public final float getEfficiency(){
+        return Mathf.maxZero(Attribute.light.env() +
+                    (state.rules.lighting ?
+                        1f - state.rules.ambientLight.a :
+                        1f
+                    )) : 0f;
+      }
+    }
+
+  
+}
