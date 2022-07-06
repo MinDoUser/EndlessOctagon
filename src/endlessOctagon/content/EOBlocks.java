adcf14adea70e5ecdde6e3fa65ad847b06878638
MinDoUser <94752757+MinDoUser@users.noreply.github.com>
2022-07-06 17:28:50 +0200

    Update EOBlocks.java

--- src/endlessOctagon/content/EOBlocks.java
+++ src/endlessOctagon/content/EOBlocks.java
@@ -62,7 +62,7 @@ public final class EOBlocks implements ContentList{
             collidesAir = true;
     }};
     crowl = new ItemTurret("crowl"){{
-            requirements(Category.turret, with(Items.copper, 135, Items.lead, 50, EOItems.oxa, 125, Item.titanium, 15));
+            requirements(Category.turret, with(Items.copper, 135, Items.lead, 50, EOItems.oxa, 125, Items.titanium, 15));
             ammo(
                 EOItems.oxa, CROWL_BULLET
             );
