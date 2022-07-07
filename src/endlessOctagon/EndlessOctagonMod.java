7345ce718cb4251664f66c8ff5b9766ac96ac8a2
MinDoUser <94752757+MinDoUser@users.noreply.github.com>
2022-07-07 12:12:01 +0200

    Update EndlessOctagonMod.java

--- src/endlessOctagon/EndlessOctagonMod.java
+++ src/endlessOctagon/EndlessOctagonMod.java
@@ -1,17 +1,42 @@
 package endlessOctagon;
 
 import arc.util.*;
+import arc.scene.ui.layout.Table;
 
 import mindustry.mod.*;
+import mindustry.ui.dialogs.*;
 
+import endlessOctagon.content.*;
+import endlessOctagon.util.ui.*;
 import endlessOctagon.content.*;
 
 public class EndlessOctagonMod extends Mod{
+    
+    public static final ObjectLog[] CURRENT_LOGS = {
+        new ObjectLog(EOBlocks.oxa, "A new item", "New:");
+    };
 
     public EndlessOctagonMod(){
       Log.info("Mod constructor loaded...");
+        Events.on(ClientLoadEvent.class, e -> {
+            //show dialog upon startup
+            Time.runTask(10f, () -> {
+                startScreen();
+            });
+        });
     }
   
+    public void startScreen(){
+        BaseDialog dialog = new BaseDialog("");
+        Table cont = dialog.cont;
+        for(ObjectLog log : CURRENT_LOGS){
+            cont.add(log.buildTable());
+            cont.row();
+            cont.row();
+        }
+        cont.button("I know", dialog::hide).size(100f, 50f);
+    }
+    
   @Override
   public void loadContent(){
         new EOItems().load();
