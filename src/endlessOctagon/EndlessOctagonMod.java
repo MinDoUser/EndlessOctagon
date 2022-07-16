a2089a51183ccdac73b367976e17578a648f5c42
MinDoUser <94752757+MinDoUser@users.noreply.github.com>
2022-07-16 10:37:00 +0200

    Update EndlessOctagonMod.java

--- src/endlessOctagon/EndlessOctagonMod.java
+++ src/endlessOctagon/EndlessOctagonMod.java
@@ -19,11 +19,11 @@ import endlessOctagon.util.ui.*;
 import endlessOctagon.util.*;
 import endlessOctagon.content.*;
 
-public class EndlessOctagonMod extends Mod{
+public final class EndlessOctagonMod extends Mod{
     
     public BaseDialog changeDialog;
     
-    public ObjectLog[] CURRENT_LOGS;
+    public ObjectLog[] CURRENT_LOGS_V_1_0_1;
 
     public EndlessOctagonMod(){
       Log.info("Mod constructor loaded...");
@@ -35,10 +35,10 @@ public class EndlessOctagonMod extends Mod{
             });
         });
     }
-  	/** ONLY ON STARTUP! ?*/
-    public final void changeScreen(){
-	    if(Core.settings.getBool("hidestartlog", false))return; //no.
-        CURRENT_LOGS = new ObjectLog[]{
+	
+	
+    public final void loadChangeDialog(){
+	    CURRENT_LOGS_V_1_0_1 = new ObjectLog[]{
                          new ObjectLog(EOItems.oxa){{
                               description = "A new Item";
                               type = ObjectLog.NEW;
@@ -50,17 +50,24 @@ public class EndlessOctagonMod extends Mod{
         changes.add("1.0.1", Styles.techLabel);
 	changes.image().growX();
 	changes.row();
-        for(ObjectLog log : CURRENT_LOGS){
+        for(ObjectLog log : CURRENT_LOGS_V_1_0_1){
             changes.add(log.buildTable());
             changes.row();
-	    changes.image().growX();
-            changes.row();
+	    //changes.image().growX();
+            //changes.row();
         }
 	addLog(changes, "Added Change Log", Icon.add);
 	addLog(changes, "Added new button to open change log. \n You can find it here: Settings -> Game -> Change Log", Icon.wrench);
+	addLog(changes, "Added a option to hide startup dialog. \n You can find it here: Settings -> Game -> Mod Settings", Icon.wrench);
 	cont.pane(changes);
         cont.row();
         changeDialog.buttons.button("Ok!", changeDialog::hide).size(100f, 50f);
+    }
+
+  	/** ONLY ON STARTUP! ?*/
+    public final void changeScreen(){
+	    if(Core.settings.getBool("hidestartlog", false))return; //no.
+        
         changeDialog.show();
     }
 	/**
@@ -70,7 +77,7 @@ public class EndlessOctagonMod extends Mod{
 	    Table table = new Table();
 	    table.row();
 	    table.image(icon);
-	    table.add(log);
+	    table.add("  "+log);
 	    table.image().growX();
 	    t.add(table).row();
     }
@@ -83,7 +90,7 @@ public class EndlessOctagonMod extends Mod{
 		settingTable.game.row();
         settingTable.game.button("Change Log", Icon.info, ()->{
             changeDialog.show();
-        }).size(300f, 100f);
+        }).size(250f, 100f);
     }
     
   @Override
