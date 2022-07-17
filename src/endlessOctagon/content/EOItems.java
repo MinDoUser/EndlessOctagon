fe37d71c7b16f67a816c14bf595df7d2f32627e8
MinDoUser <94752757+MinDoUser@users.noreply.github.com>
2022-07-17 12:13:18 +0200

    Update EOItems.java

--- src/endlessOctagon/content/EOItems.java
+++ src/endlessOctagon/content/EOItems.java
@@ -4,12 +4,12 @@ import arc.graphics.*;
 import mindustry.ctype.*;
 import mindustry.type.*;
 
-public class EOItems implements ContentList{
+public class EOItems implements endlessOctagon.util.Loadable{
   /** Items color*/
   public static final Color 
     OXA_COLOR = Color.valueOf("eab678");
     public static Item oxa;
-  public final void load(){
+  public final void loadObject(){
     oxa = new Item("oxa", OXA_COLOR){{
             hardness = 1;
             cost = 0.75f;
