08ce1e3b6ee59aa8148d72ad2419246509902c8c
MinDoUser <94752757+MinDoUser@users.noreply.github.com>
2022-07-18 20:41:43 +0200

    Update EOItems.java

--- src/endlessOctagon/content/EOItems.java
+++ src/endlessOctagon/content/EOItems.java
@@ -1,19 +1,34 @@
 package endlessOctagon.content;
 
 import arc.graphics.*;
+import arc.struct.*;
+
 import mindustry.ctype.*;
 import mindustry.type.*;
+import mindustry.content.*;
 
 public class EOItems implements endlessOctagon.util.Loadable{
+  public static Seq<Item> eravirItems = new Seq<>();
   /** Items color*/
   public static final Color 
-    OXA_COLOR = Color.valueOf("eab678");
-    public static Item oxa;
+    OXA_COLOR = Color.valueOf("eab678"),
+    GENIUM_COLOR = Color.valueOf("eab678");
+  
+    public static Item 
+      oxa, genium;
   public final void loadObject(){
     oxa = new Item("oxa", OXA_COLOR){{
             hardness = 1;
             cost = 0.75f;
             alwaysUnlocked = false;
         }};
+     genium = new Item("genium", GENIUM_COLOR){{
+            hardness = 1;
+            cost = 1.2f;
+            alwaysUnlocked = false;
+        }};
+    
+    
+    eravirItems.addAll(oxa, genium, Item.metaglass, Item.graphite, Item.thorium, Item.silicon, Item.titanium);
   }
 }
