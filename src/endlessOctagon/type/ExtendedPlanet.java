1aa61ae9c46ae3667eb03d8553d3afbca3e9c6e7
MinDoUser <94752757+MinDoUser@users.noreply.github.com>
2022-07-19 14:08:32 +0200

    Create ExtendedPlanet.java

---
+++ src/endlessOctagon/type/ExtendedPlanet.java
@@ -0,0 +1,44 @@
+package endlessOctagon.type;
+
+import mindustry.type.*;
+import mindustry.world.meta.*;
+
+import arc.scene.ui.layout.*;
+import arc.struct.*;
+/** A planet with stats, yeah!*/
+public class ExtendedPlanet extends Planet{
+  public static final Stat
+    isAccessible = new Stat("accessible"), dayNightCycle = new Stat("daynightcycle"),atmosphere = new Stat("atmosphere"),
+    spawnsWavesInBackground = new Stat("spawnswaves"), hasInvations = new Stat("hasinvations"), clearSectors = new Stat("clearsectors),
+    planetParent = new Stat("planetparent"), hasOwnTechTree = new Stat("hastechtree"), planetHiddenItems = new Stat("hiddenitems");
+                                                                                                                         
+    public ExtendedPlanet(String name, Planet parent, float radius){
+      super(name, parent, radius);
+    }
+  //:O Stats?!, ye, keep cool.
+  @Override
+  public void setStats(){
+    final int MAX_PER_ROW = 5;
+    stats.add(isAccessible, super.accesible);
+    stats.add(dayNightCycle, super.updateLighting);
+    stats.add(atmosphere, super.hasAtmosphere);
+    stats.add(spawnsWavesInBackground, super.allowWaveSimulation);
+    stats.add(hasInvations, super.allowSectorInvasion);
+    stats.add(clearSectors, super.clearSectorOnLose);
+    stats.add(planetParent, (super.parent == null)? "[lightgrey]<none>[]":super.parent.localizedName);
+    stats.add(hasOwnTechTree, super.techtree != null);
+    stats.add(planetHiddenItems, (table)->{
+      Seq<Item> hidden = super.hiddenItems;
+      int i = 0;
+      hidden.each(i->{
+        i++;
+        table.image(i.uiIcon);
+        table.add(i.localizedName);
+        table.add("  ");
+        if(i%MAX_PER_ROW){
+          table.row();
+        }
+      });
+    });
+  }
+}
