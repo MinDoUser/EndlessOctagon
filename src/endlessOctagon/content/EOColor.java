c5aabb6d189872f6aeb342a2f57dfd4b378acc6e
MinDoUser <94752757+MinDoUser@users.noreply.github.com>
2022-07-21 18:04:43 +0200

    Update EOColor.java

--- src/endlessOctagon/content/EOColor.java
+++ src/endlessOctagon/content/EOColor.java
@@ -2,7 +2,7 @@ package endlessOctagon.content;
 
 import arc.graphics.*;
 
-public final EOColor {
+public final class EOColor {
   public static final Color 
     lightBlue = new Color(0x8aa3f4ff),
     darkBlue = new Color(0x6974c4ff)
