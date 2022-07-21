7d4f354b1a9a5ed933dad59c025ef8e1a28a9403
MinDoUser <94752757+MinDoUser@users.noreply.github.com>
2022-07-21 18:24:07 +0200

    Update n rename

--- src/endlessOctagon/content/EOPal.java
+++ src/endlessOctagon/content/EOPal.java
@@ -2,7 +2,7 @@ package endlessOctagon.content;
 
 import arc.graphics.*;
 
-public final class EOColor {
+public final class EOPal {
   public static final Color 
     lightBlue = new Color(0x8aa3f4ff),
     darkBlue = new Color(0x6974c4ff);
