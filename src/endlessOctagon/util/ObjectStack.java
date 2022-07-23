7074cf2430d044a0b1b4a152b646f34fff441e84
MinDoUser <94752757+MinDoUser@users.noreply.github.com>
2022-07-23 09:41:19 +0200

    Create ObjectStack.java

---
+++ src/endlessOctagon/util/ObjectStack.java
@@ -0,0 +1,36 @@
+package endlessOctagon.util;
+// Easy.
+/**A stack of any object.*/
+public class ObjectStack<T> {
+  public final T object;
+  /** The amount*/
+  public int amount = 0;
+  
+  public ObjectStack(T object, int amount){
+    this.object = object;
+    this.amount = amount;
+  }
+  
+  public ObjectStack(ObjectStack<E super T> other){
+    this.object = other.object;
+    this.amount = other.amount;
+  }
+  /** Multiplies the amount of this stack by param {@code mult}*/
+  public ObjectStack mulBy(float mult){
+    float aMult = (float)Math.abs(mult);
+    this.amount = (int)Math.round(this.amount * aMult);
+    return this; //Chain, ye
+  }
+  
+  @Override
+  public boolean equals(Object other){
+    if(other instanceof ObjectStack o){
+      if((o.amount == this.amount) && (o.object.equals(this.object))return true;
+    }else return false;
+  }
+  
+  @Override
+  public void toString(){
+    return "ObjectStack:" object +' '+amount;
+  }
+}
