54a092ea728801fd488c57c3fadae43f35e8e896
MinDoUser <94752757+MinDoUser@users.noreply.github.com>
2022-09-02 20:02:38 +0200

    Update EOItems.java

--- src/endlessOctagon/content/EOItems.java
+++ src/endlessOctagon/content/EOItems.java
@@ -11,11 +11,16 @@ public class EOItems implements endlessOctagon.util.Loadable{
   public static Seq<Item> eravirItems = new Seq<>();
   /** Items color*/
   public static final Color 
-    OXA_COLOR = Color.valueOf("eab678"),
-    GENIUM_COLOR = Color.valueOf("eab678");
+    OXA_COLOR = Color.valueOf("eab678"), 
+    GENIUM_COLOR = Color.valueOf("eab678"),
+  
+    C_IRON = Color.valueOf("b364cd"),
+    C_MULTI_STEEL = Color.valueOf("daddea");
   
     public static Item 
-      oxa, genium;
+      oxa, genium,
+  
+    iron, multiSteel;
   public final void loadObject(){
     oxa = new Item("oxa", OXA_COLOR){{
             hardness = 1;
@@ -29,6 +34,18 @@ public class EOItems implements endlessOctagon.util.Loadable{
         }};
     
     
-    eravirItems.addAll(oxa, genium, Items.metaglass, Items.graphite, Items.thorium, Items.silicon, Items.titanium);
+    iron = new Item("iron", C_IRON){{
+      hardness = 1;
+      cost = 0.88f;
+      alwaysUnlocked = true;
+    }};
+    
+    multiSteel = new Item("multi-steel", C_MULTI_STEEL){{
+      hardness = 1;
+      cost = 1.25f;
+      alwaysUnlocked = false;
+    }};
+    
+    eravirItems.addAll(iron, multiSteel, Items.graphite, Items.silicon);
   }
 }
