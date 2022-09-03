9919d775d43859f774e64f06da953f763d479136
MinDoUser <94752757+MinDoUser@users.noreply.github.com>
2022-09-03 14:50:44 +0200

    Update EOBlocks.java

--- src/endlessOctagon/content/EOBlocks.java
+++ src/endlessOctagon/content/EOBlocks.java
@@ -35,6 +35,7 @@ import mindustry.world.blocks.units.*;
 import mindustry.world.consumers.*;
 import mindustry.world.draw.*;
 import mindustry.world.meta.*;
+import mindustry.content.*;
 import static mindustry.content.Fx.*;
 
 import static mindustry.type.ItemStack.*;
