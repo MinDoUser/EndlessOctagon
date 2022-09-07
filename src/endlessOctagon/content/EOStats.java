091f705fe7818ccada52c508b2774c79c880c6a0
MinDoUser <94752757+MinDoUser@users.noreply.github.com>
2022-09-07 12:20:42 +0200

    Update EOStats.java

--- src/endlessOctagon/content/EOStats.java
+++ src/endlessOctagon/content/EOStats.java
@@ -3,8 +3,15 @@ package endlessOctagon.content;
 import endlessOctagon.util.*;
 
 import mindustry.world.meta.*;
+import mindustry.ctype.*;
 
 public final class EOStats {
   public static final Stat 
     special = new Stat("special");
+  
+  public static StatValue createContentValue(UnlockableContent effect){
+    return table -> {
+      //TODO: Create
+    }
+  }
 }
