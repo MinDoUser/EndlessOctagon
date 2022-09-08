62c604d5fa9578434e35532dca60c21722242d60
MinDoUser <94752757+MinDoUser@users.noreply.github.com>
2022-09-08 16:49:33 +0200

    Update UnitGateDialog.java

--- src/endlessOctagon/util/ui/UnitGateDialog.java
+++ src/endlessOctagon/util/ui/UnitGateDialog.java
@@ -1,8 +1,16 @@
 package endlessOctagon.util.ui;
 
 import mindustry.ui.dialogs.*;
+import mindustry.ui.*;
+import mindustry.graphics.*;
+import mindustry.gen.*;
+import mindustry.*;
 
 import arc.util.*;
+import arc.scene.ui.layout.*;
+import arc.scene.ui.*;
+import arc.graphics.*;
+import arc.*;
 
 import endlessOctagon.world.blocks.units.*;
 import endlessOctagon.world.blocks.units.UnitGate.*;
@@ -11,10 +19,14 @@ public class UnitGateDialog extends BaseDialog {
   
   protected final UnitGateBuild unitGate;
   
+  protected Seq<UnitBuildPlan> plansSeq;
+  
   public UnitGateDialog(UnitGateBuild build){
     super("");
     if(build == null)throw new NullPointerException("build was null");
     unitGate = build;
+    UnitGate gate = (UnitGate)unitGate.block;
+    plansSeq = gate.plans;
     
     addCloseButton();
     
@@ -23,12 +35,68 @@ public class UnitGateDialog extends BaseDialog {
   
   public void rebuild(){
     cont.clear();
-    UnitGate gate = (UnitGate)unitGate.block;
-    if(gate.plans.size <= 0){
-      cont.add("Thegate has no plans!");
+    //UnitGate gate = (UnitGate)unitGate.block;
+    if(plansSeq.size <= 0){
+      cont.add("The gate has no plans!");
       return;
     }
     
+    
+    
+  }
+  
+  public static Table createTableOfPlan(UnitBuildPlan plan){
+    Table rTable = new Table();
+    rTable.setBackground(Tex.whiteui);
+    rTable.setColor(Pal.darkestGray);
+    rTable.table(t -> {
+      t.image(plan.unit.uiIcon).size(Vars.iconLarge).left();
+      t.row();
+      t.add(plan.unit.localizedName).left();
+      t.row();
+      t.add(Strings.fixed(plan.time/60f, 1)+Core.bundle.get("unit.seconds").color(Color.lightGray);
+    }).width(450).left();
+      rTable.table(t -> {
+        t.button(Icon.infoCircle, ()->{
+          var unit = plan.unit;
+          BaseDialog dialog = new BaseDialog(unit.localizedName);
+          dialog.addCloseButton();
+          dialog.cont.table(t -> {
+            t.left();
+            t.image(unit.uiIcon)size(Vars.iconLarge);
+            t.row();
+            t.add(unit.loclaizedName, Styles.techLabel);
+            t.image().growX().color(Color.white);
+          }).width(300);
+          dialog.cont.row();
+          if(unit.description != null)dialog.cont.add(unit.description);
+          dialog.cont.row();
+          dialog.cont.table(t -> {
+            t.add(Core.bundle.get("stat.buildcost"));
+            t.table(req -> {
+              req.right();
+              for(int i = 0; i < plan.requirements.length; i++){
+              if(i % 6 == 0){
+                 req.row();
+              }
+
+              var stack = plan.requirements[i];
+              req.add(new ItemDisplay(stack.item, stack.amount, false)).pad(5);
+              }
+            }).right().grow().pad(10f);
+            dialog.show();
+          }).tooltip("Info");
+          
+          t.button(Icon.units, ()-> {
+            unitGate.configure(plan.unit);
+          }).tooltip("Spawn");
+          t.row();
+          final String BUILD_STRING = Core.bundle.get("canbuild", "Can Build")+":";
+          boolean build = unitGate.items.has(plans.requirements);
+          t.add(!build ? Core.bundle.get("no","No") : Core.bundle.get("yes","Yes").right();
+        }
+      });
+    return rTable;
   }
   /*
   public void show(UnitGateBuild build){
