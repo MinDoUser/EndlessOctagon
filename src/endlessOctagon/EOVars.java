5e8bf08963db99a44ffba1dbdaebe3614da6e726
MinDoUser <94752757+MinDoUser@users.noreply.github.com>
2022-09-16 13:06:43 +0200

    Update EOVars.java

--- src/endlessOctagon/EOVars.java
+++ src/endlessOctagon/EOVars.java
@@ -5,8 +5,15 @@ import endlessOctagon.util.*;
 
 public final class EOVars {
   public static final MapInfoDialog mapInfo = new MapInfoDialog();
-  
-  public static final char warningChar = '\u26A0';
+  //Theese are funny.
+  public static final char
+    warningChar = '\u26A0',
+    dice1 = '\u2680',
+    dice2 = '\u2681',
+    dice3 = '\u2682',
+    dice4 = '\u2683',
+    dice5 = '\u2684'
+    dice6 = '\u2685';
   
   //public static final UnitGateDialog unitGateDialog = new UnitGateDialog();
 }
