1fc6e139fa5fbd8d6fb283b8115c035a62ab7b37
MinDoUser <94752757+MinDoUser@users.noreply.github.com>
2022-10-11 22:30:43 +0200

    Update EOUnitType.java

--- src/endlessOctagon/type/EOUnitType.java
+++ src/endlessOctagon/type/EOUnitType.java
@@ -1,11 +1,59 @@
 package endlessOctagon.type;
 
 import mindustry.type.*;
+import mindustry.gen.*;
+
+import arc.struct.*;
+
+import endlessOctagon.entities.*;
 
 public class EOUnitType extends UnitType {
   
+  //Rotors.
+  /** Whether this unit has rotors or not */
+  public boolean hasRotors = false;
+  public Seq<UnitRotor> rotors = new Seq<>(2);
+  
+  
   public EOUnitType(String name){
     super(name);
+    
+    contructor = UnitEntity::create; //Prevent nulls. o_o
+  }
+  
+  public void addRotors(UnitRotor... rotors){
+    rotors.addAll(rotors);
+  }
+  
+  @Override
+  public void load(){
+    super.load();
+    if(rotors != null && hasRotors){
+      for(var rotor : rotors){
+        if(rotor != null)rotor.loadRegions();
+      }
+    }
+  }
+  
+  @Override
+  public void update(Unit unit){
+    super.update(unit);
+    
+    if(rotors != null && hasRotors){
+      for(var rotor : rotors){
+        if(rotor != null)rotor.update(unit);
+      }
+    }
+  }
+  
+  @Override
+  public void draw(Unit unit){
+    super.draw(unit);
+    if(rotors != null && hasRotors){
+      for(var rotor : rotors){
+        if(rotor != null)rotor.draw(unit);
+      }
+    }
   }
   
 }
