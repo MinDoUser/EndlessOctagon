3b8f8918f6ef168cddd24db4d0b5462741c96d42
MinDoUser <94752757+MinDoUser@users.noreply.github.com>
2022-10-26 15:21:48 +0200

    Update EravirPlanetGenerator.java

--- src/endlessOctagon/maps/planet/EravirPlanetGenerator.java
+++ src/endlessOctagon/maps/planet/EravirPlanetGenerator.java
@@ -1,14 +1,31 @@
 package endlessOctagon.maps.planet;
 
-import mindustry.maps.generators.*;
-import mindustry.world.*;
+import arc.graphics.*;
+import arc.math.*;
+import arc.math.geom.*;
+import arc.util.*;
+import arc.util.noise.*;
+import mindustry.ai.*;
 import mindustry.content.*;
+import mindustry.game.*;
+import mindustry.maps.generators.*;
+import mindustry.type.*;
+import mindustry.world.*;
+import mindustry.world.blocks.environment.*;
+import mindustry.world.meta.*;
+
+import static mindustry.Vars.*;
 
 
 import endlessOctagon.content.*;
 
 public class EravirPlanetGenerator extends PlanetGenerator {
   
+  protected Block[] blocks = {EOBlocks.lightStone, EOBlocks.ironStone, EOBlocks.lightStone}; //TODO More environment
   
+  @Override
+  public Color getColor(Vec3 location){
+    return Color.white; //TODO fix when having blocks.
+  }
   
 }
