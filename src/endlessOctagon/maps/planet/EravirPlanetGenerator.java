4ef63d96d8e130e1f5bcecfc031db42f21354e23
MinDoUser <94752757+MinDoUser@users.noreply.github.com>
2022-10-26 14:51:36 +0200

    Create EravirPlanetGenerator.java

---
+++ src/endlessOctagon/maps/planet/EravirPlanetGenerator.java
@@ -0,0 +1,14 @@
+package endlessOctagon.maps.planet;
+
+import mindustry.maps.generator.*;
+import mindustry.world.*;
+import mindustry.content.*;
+
+
+import endlessOctagon.content.*;
+
+public class EravirPlanetGenerator extends PlanetGenerator {
+  
+  
+  
+}
