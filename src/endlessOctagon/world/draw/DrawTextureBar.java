45da4a6c6c19b13307a6a9e016f1b573b40b07e5
MinDoUser <94752757+MinDoUser@users.noreply.github.com>
2022-10-26 13:02:00 +0200

    Create DrawTextureBar.java

---
+++ src/endlessOctagon/world/draw/DrawTextureBar.java
@@ -0,0 +1,46 @@
+package endlessOctagon.world.draw;
+
+import mindustry.world.draw.*;
+import mindustry.world.*;
+import mindustry.gen.*;
+
+import arc.math.*;
+import arc.Core;
+import arc.graphics.g2d.*;
+//TODO: Still not done!
+public class DrawTextureBar extends DrawBlock {
+  public TextureRegion bar;
+  /** Whether to draw a vertical/horizontal bar or not*/
+  public boolean vertical = true, horizontal = false;
+  /** Used for not full-square sprites to avoid bar is drawen outside the block.*/
+  public float startX = 0, startY = 0;
+  
+  @Override
+  public void draw(Building build){
+    if(!vertical && !horizontal)return; //If nothing should be drawn, we don't need to do something.
+    Draw.alpha(build.warmup());
+    float rotation;
+    float dx,dy;
+    if(vertical){
+      rotation = 90;
+      dx = build.x;
+      dy = build.y;
+    }else if(horizontal){
+      rotation = 0;
+      dx = build.x + Mathf.sin(build.totalProgress(), 6f, Vars.tilesize / 3f * build.block.size)+startX;
+      dy = build.y;
+    }
+    
+    
+    
+    Draw.rect(bar,dx,dy, rotation);
+    
+    Draw.reset();
+  }
+  
+  @Override
+  public void load(Block block){
+    this.bar = Core.atlas.find(block.name+"-bar");
+  }
+  
+}
