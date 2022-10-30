c423dc7a7f397e3ea4bc7ac765d38d0769f55fce
MinDoUser <94752757+MinDoUser@users.noreply.github.com>
2022-10-30 18:31:54 +0100

    Add files via upload

---
+++ src/endlessOctagon/util/ui/CodeDialog.java
@@ -0,0 +1,42 @@
+package endlessOctagon.util.ui;
+
+import mindustry.ui.dialogs.*;
+import mindustry.Vars;
+
+import arc.scene.ui.*;
+import arc.Core;
+import arc.input.*;
+
+/** Only for testing and debug*/
+public final class CodeDialog extends BaseDialog {
+	
+	private TextArea codeArea;
+	private Label outputLabel = new Label("");
+	
+	public CodeDialog(){
+		super("Code Dialog");
+		
+		Runnable runAction = ()->{
+					String out = Vars.mods.getScripts().runConsole(codeArea.getText());
+					outputLabel.setText(out);
+				};
+		this.cont.table(t -> {
+				codeArea = t.add(new TextArea("")).size(540, 450).get();
+				t.row();
+				t.add(outputLabel);
+				t.row();
+				t.button("Run!", runAction).size(100,50);
+				t.button("Clear!", ()->{
+						codeArea.setText("");
+				}
+		}
+		
+		keyDown(key -> {
+				if(key == KeyCode.n){
+					Core.app.post(this::hide);
+				}
+		});
+		
+		this.addCloseButton();
+	}
+}
